346e1978479b51baa74eb1d78bc12d4f71c592f4
Emilio Peláez <me@epelaez.net>
2021-02-20 00:35:08 +0100

    Keypad style

--- Project/PrivateVault App/Code/UI/KeypadView.swift
+++ Project/PrivateVault App/Code/UI/KeypadView.swift
@@ -17,37 +17,37 @@ struct KeypadView: View {
 	}
 	
 	var input: (Int) -> Void
+	var delete: () -> Void
 	
-    var body: some View {
+	var body: some View {
 		LazyVGrid(columns: columns, alignment: .center, content: {
 			ForEach(1..<10){ index in
-				KeyButton(title: String(index)) {
+				KeyButton(title: Text("\(index)"), color: Color(#colorLiteral(red: 0.7065681379, green: 0.6965085175, blue: 0.7033597253, alpha: 1))) {
 					input(index)
 				}
 			}
-			
 			Spacer()
-			KeyButton(title: String(10)) {
+			KeyButton(title: Text("0"), color: Color(#colorLiteral(red: 0.7065681379, green: 0.6965085175, blue: 0.7033597253, alpha: 1))) {
 				
 			}
 			.aspectRatio(1, contentMode: .fill)
 			.clipShape(Circle())
-			KeyButton(title: "Clear"){
-				
+			KeyButton(title: Image(systemName: "delete.left"), color: Color(#colorLiteral(red: 0.8059458137, green: 0.1390043199, blue: 0.1966293752, alpha: 1))){
+				delete()
 			}
-			
 		})
-    }
+	}
 }
 
-struct KeyButton: View {
-	var title: String
+struct KeyButton<Body: View>: View {
+	var title: Body
+	var color: Color
 	var action: () -> Void
 	var body: some View {
 		Button(action: action, label: {
 			ZStack {
-				Color(#colorLiteral(red: 0.7065681379, green: 0.6965085175, blue: 0.7033597253, alpha: 1))
-				Text("\(title)")
+				color
+				title
 					.font(.largeTitle)
 					.foregroundColor(.white)
 			}
@@ -59,9 +59,9 @@ struct KeyButton: View {
 
 
 struct LockView_Previews: PreviewProvider {
-    static var previews: some View {
-		KeypadView(input: { _ in
-			
-		})
-    }
+	static var previews: some View {
+		KeypadView { _ in
+		} delete: {
+		}
+	}
 }

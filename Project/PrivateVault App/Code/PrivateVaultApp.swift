18dc73f18d9545d0c1a5dc3f87b537776838f72d
Emilio Peláez <me@epelaez.net>
2021-02-19 22:36:42 +0100

    Create project

---
+++ Project/PrivateVault App/Code/PrivateVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateVaultApp.swift
+//  PrivateVault
+//
+//  Created by Emilio Peláez on 19/2/21.
+//
+
+import SwiftUI
+
+@main
+struct PrivateVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

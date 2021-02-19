aa4043167dd73e50e2bd89a3439879e750175c98
Emilio Peláez <me@epelaez.net>
2021-02-19 22:36:42 +0100

    Create project

---
+++ Project/PrivateVault App/Code/UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrivateVault
+//
+//  Created by Emilio Peláez on 19/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

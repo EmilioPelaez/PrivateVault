bf6ebad13920c168c1a0862f92415a55a937d7d5
Emilio Peláez <me@epelaez.net>
2021-02-19 23:31:38 +0100

    Grid with content

--- Project/PrivateVault App/Code/UI/GalleryView.swift
+++ Project/PrivateVault App/Code/UI/GalleryView.swift
@@ -17,19 +17,34 @@ struct GalleryView: View {
 		]
 	}
 	
-	var data: [Item] = [Color.red, .green, .blue, .red, .green, .blue, .red, .green, .blue]
+	@State var contentMode: ContentMode = .fill
+	
+	var data: [Item] = (1...6)
+		.map { "file\($0)" }
+		.map { Image($0) }
 		.map(Item.init)
 	
 	var body: some View {
 		ScrollView {
 			LazyVGrid(columns: columns) {
-				ForEach(data) {
-					$0.color
-						.aspectRatio(1, contentMode: .fill)
+				ForEach(data) { item in
+					Color.red.aspectRatio(1, contentMode: .fill)
+						.overlay(
+							item.image
+								.resizable()
+								.aspectRatio(contentMode: contentMode)
+						)
+						.clipped()
+						.onTapGesture { self.display(item) }
 				}
 			}
 		}
 	}
+	
+	func display(_ item: Item) {
+		print(item.id)
+	}
+	
 }
 
 struct GalleryView_Previews: PreviewProvider {

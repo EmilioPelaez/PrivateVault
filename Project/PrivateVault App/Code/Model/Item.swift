ffe8ad204bc4593f4f201f8bc9babbe0ddb6a0b1
Emilio Peláez <me@epelaez.net>
2021-02-20 01:13:14 +0100

    WIP improve Item to store image

--- Project/PrivateVault App/Code/Model/Item.swift
+++ Project/PrivateVault App/Code/Model/Item.swift
@@ -6,15 +6,44 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct Item: Identifiable {
-	var id: String = UUID().uuidString
 	
-	let image: Image
+	let id: String
+	let title: String
+	let url: URL
+	
+	private var _placeholder: Image?
+	var placeholder: Image {
+		if let _placeholder = _placeholder { return _placeholder }
+		return Image(systemName: "xmark.octagon")
+	}
 }
 
 extension Item {
-	init(image: Image) {
-		self.image = image
+	init(image: UIImage) {
+		let id = UUID().uuidString
+		self.id = id
+		self.title = ""
+		self._placeholder = Image(uiImage: image)
+		
+		let data = image.pngData()
+		let folder = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+			.appendingPathComponent("data")
+		let url = folder
+			.appendingPathComponent(id)
+			.appendingPathExtension("png")
+		do {
+			try FileManager.default.createDirectory(at: folder, withIntermediateDirectories: true, attributes: nil)
+		} catch {
+			print("Uh oh", error)
+		}
+		
+		FileManager.default.createFile(atPath: url.absoluteString, contents: data, attributes: nil)
+		
+		self.url = url
 	}
 }
+
+
---
+++ Project/PrivateVault App/Code/UI/GalleryView.swift
@@ -0,0 +1,55 @@
+//
+//  GalleryView.swift
+//  PrivateVault
+//
+//  Created by Emilio Peláez on 19/2/21.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+	@State var contentMode: ContentMode = .fill
+	@State var selectedItem: Item?
+
+	var columns: [GridItem] {
+		[
+			GridItem(.flexible()),
+			GridItem(.flexible()),
+			GridItem(.flexible())
+		]
+	}
+	
+	var data: [Item] = (1...6)
+		.map { "file\($0)" }
+		.compactMap { UIImage(named: $0) }
+		.map(Item.init)
+	
+	var body: some View {
+		ScrollView {
+			LazyVGrid(columns: columns) {
+				ForEach(data) { item in
+					Color.red.aspectRatio(1, contentMode: .fill)
+						.overlay(
+							item.placeholder
+								.resizable()
+								.aspectRatio(contentMode: contentMode)
+						)
+						.clipped()
+						.onTapGesture {  }
+				}
+			}
+		}
+		.navigation(item: $selectedItem, destination: quickLookView)
+	}
+
+	@ViewBuilder
+	func quickLookView(_ item: Item) -> some View {
+		QuickLookView(title: item.id.description, url: URL(string: "")!)
+	}
+}
+
+struct GalleryView_Previews: PreviewProvider {
+	static var previews: some View {
+		GalleryView()
+	}
+}

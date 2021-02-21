4264b1de94761477e6a6d8b78ab0ad7e6de2ec56
Emilio Peláez <me@epelaez.net>
2021-02-21 19:04:15 +0100

    WIP about screen

---
+++ Project/PrivateVault App/Code/UI/Settings/AboutView.swift
@@ -0,0 +1,134 @@
+//
+//  AboutView.swift
+//  PrivateVault
+//
+//  Created by Emilio Peláez on 21/2/21.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+	var body: some View {
+		NavigationView {
+			ScrollView(.vertical, showsIndicators: true) {
+				VStack(spacing: 0) {
+					HStack(alignment: .top, spacing: 20) {
+						Image("Icon")
+							.resizable()
+							.frame(width: 120, height: 120)
+							.clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+						VStack(alignment: .leading, spacing: 8) {
+							Text("Private Vault")
+								.bold()
+								.font(.title)
+							Text("Version 0.0.1")
+								.font(.subheadline)
+						}
+						.padding(.top, 10)
+						Spacer()
+					}
+					.padding()
+					VStack(alignment: .leading, spacing: 8) {
+						Text("Created by")
+							.font(.headline)
+						CreatorRow(name: "Emilio Peláez", title: "Programmer", links: [
+							.website("http://emiliopelaez.me"),
+							.twitter("http://twitter.com/EmilioPelaez"),
+							.appStore("https://apps.apple.com/us/developer/emilio-pelaez/id408763858")
+						])
+					}
+					.padding()
+				}
+			}
+			.navigationTitle("About")
+		}
+	}
+	
+	struct CreatorRow: View {
+		let name: String
+		let title: String
+		let links: [LinkType]
+		
+		var body: some View {
+			HStack(spacing: 0) {
+				VStack(alignment: .leading) {
+					Text(name)
+						.bold()
+						.font(.title2)
+					Text(title)
+						.font(.subheadline)
+				}
+				Spacer()
+				HStack(spacing: 4) {
+					ForEach(links, id: \.systemName) { LinkButton(linkType: $0) }
+				}
+			}
+			.padding()
+			.background(Color(.tertiarySystemFill))
+			.cornerRadius(15)
+		}
+		
+		enum LinkType {
+			case twitter(String)
+			case website(String)
+			case appStore(String)
+			
+			var systemName: String {
+				switch self {
+				case .twitter: return "message.fill"
+				case .website: return "globe"
+				case .appStore: return "apps.iphone"
+				}
+			}
+			
+			var color: Color {
+				switch self {
+				case .twitter: return Color(red: 0.11, green: 0.63, blue: 0.95)
+				case .website: return .blue
+				case .appStore: return .black
+				}
+			}
+			
+			var size: CGFloat {
+				switch self {
+				case .twitter: return 20
+				case .website: return 30
+				case .appStore: return 26
+				}
+			}
+			
+			var urlString: String {
+				switch self {
+				case .twitter(let urlString): return urlString
+				case .website(let urlString): return urlString
+				case .appStore(let urlString): return urlString
+				}
+			}
+		}
+		
+		struct LinkButton: View {
+			let linkType: LinkType
+			var body: some View {
+				Button {
+					guard let url = URL(string: linkType.urlString) else { return }
+					UIApplication.shared.open(url)
+				}
+				label: {
+					Image(systemName: linkType.systemName)
+						.frame(width: 40, height: 40)
+						.background(linkType.color)
+						.cornerRadius(20)
+						.foregroundColor(.white)
+						.font(.system(size: linkType.size, weight: .regular, design: .default))
+				}
+			}
+		}
+	}
+	
+}
+
+struct AboutView_Previews: PreviewProvider {
+	static var previews: some View {
+		AboutView()
+	}
+}

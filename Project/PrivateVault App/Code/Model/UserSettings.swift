bf7fbfba706bfbb1597da931315b6701a4ab3cd3
Emilio Peláez <me@epelaez.net>
2021-02-21 17:32:51 +0100

    Store settings in UserDefaults

--- Project/PrivateVault App/Code/Model/UserSettings.swift
+++ Project/PrivateVault App/Code/Model/UserSettings.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 final class UserSettings: ObservableObject {
 	@Published var contentMode: ContentMode = .fill
-	@Published var showDetails = true
-	@Published var passcode = ""
-	@Published var codeLength = 4
-	@Published var maxAttempts = 5
+	@Published var showDetails = UserDefaults.standard.bool(forKey: .showDetailsKey) {
+		didSet { UserDefaults.standard.set(showDetails, forKey: .showDetailsKey) }
+	}
+	//	TODO: Move this out of UserDefaults and into CoreData
+	@Published var passcode = UserDefaults.standard.object(forKey: .passcode) as? String ?? "" {
+		didSet { UserDefaults.standard.set(passcode, forKey: .passcode) }
+	}
+	@Published var codeLength = UserDefaults.standard.object(forKey: .codeLength) as? Int ?? 4 {
+		didSet { UserDefaults.standard.set(codeLength, forKey: .codeLength) }
+	}
+	@Published var maxAttempts = UserDefaults.standard.object(forKey: .maxAttempts) as? Int ?? 5 {
+		didSet { UserDefaults.standard.set(maxAttempts, forKey: .maxAttempts) }
+	}
+}
+
+fileprivate extension String {
+	static let showDetailsKey = "showDetailsKey"
+	static let passcode = "passcode"
+	static let codeLength = "codeLength"
+	static let maxAttempts = "maxAttempts"
 }

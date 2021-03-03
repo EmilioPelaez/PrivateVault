870bd915ef21d975395ebedbab6177451fdd7a72
Emilio Peláez <me@epelaez.net>
2021-03-03 10:01:11 +0100

    Remove unused url property on stored items

--- Project/PrivateVault App/Code/Model/Extensions/StoredItem+Properties.swift
+++ Project/PrivateVault App/Code/Model/Extensions/StoredItem+Properties.swift
@@ -8,23 +8,6 @@
 import SwiftUI
 
 extension StoredItem {
-	var url: URL {
-		guard let data = data, let fileExtension = fileExtension else { return URL(fileURLWithPath: "") }
-		do {
-			let folder = FileManager.default.temporaryDirectory.appendingPathComponent("data")
-			let url = folder
-				.appendingPathComponent("temp")
-				.appendingPathExtension(fileExtension)
-			
-			try FileManager.default.createDirectory(at: folder, withIntermediateDirectories: true, attributes: nil)
-			
-			try data.write(to: url)
-			return url
-		} catch {
-			return URL(fileURLWithPath: "")
-		}
-	}
-	
 	func generatePreview() -> Image? {
 		previewData.flatMap(UIImage.init).map(Image.init)
 	}
